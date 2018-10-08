fd5ec10e117aca434367ce777effd919b6311525
Joakim Wimmerstedt <joakim.wimmerstedt@gmail.com>
2018-10-08 17:02:55 +0200

    Small refactor with try/catch

--- src/services/sudoku-solver.js
+++ src/services/sudoku-solver.js
@@ -1,23 +1,29 @@
 const CheckBoardValidity = (board) => {
-  return rowsAreValid(board) && columnsAreValid(board) && groupsAreValid(board);
-};
-
-const rowsAreValid = (board) => {
-  for (let i = 0; i < board.length; i++) {
-    if ([...new Set(board[i])].length !== 9) return false;
+  try {
+    checkRowsValidity(board);
+    checkColumnsValidity(board);
+    checkGroupsValidity(board);
+    return true;
+  } catch (e) {
+    console.log(e);
+    return false;
   }
-  return true;
 };
 
-const columnsAreValid = (board) => {
+const checkRowsValidity = (board) => {
+  for (let i = 0; i < board.length; i++) {
+    if ([...new Set(board[i])].length !== 9) throw 'There are invalid rows!';
+  }
+};
+
+const checkColumnsValidity = (board) => {
   for (let i = 0; i < board.length; i++) {
     const column = board.map((row) => row[i]);
-    if ([...new Set(column)].length !== 9) return false;
+    if ([...new Set(column)].length !== 9) throw 'There are invalid columns!';
   }
-  return true;
 };
 
-const groupsAreValid = (board) => {
+const checkGroupsValidity = (board) => {
   const groupRows = [[[], [], []], [[], [], []], [[], [], []]];
   board.forEach((row, rowIndex) => {
     row.forEach((col, colIndex) => {
@@ -26,18 +32,12 @@ const groupsAreValid = (board) => {
       groupRows[groupX][groupY].push(col);
     });
   });
-  try {
-    groupRows.forEach((row) => {
-      row.forEach((group) => {
-        const set = [...new Set(group)];
-        if (set.length !== 9) throw 'Invalid';
-      });
+  groupRows.forEach((row) => {
+    row.forEach((group) => {
+      const set = [...new Set(group)];
+      if (set.length !== 9) throw 'There are invalid groups!';
     });
-  } catch (e) {
-    return false;
-  }
-
-  return true;
+  });
 };
 
 export default { CheckBoardValidity };
